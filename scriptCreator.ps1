aecebb5a410c10618fac42fd9c9057fca111e30f
William <willspri@gmail.com>
2024-07-04 15:48:27 +1200

    saves result as .py file

--- scriptCreator.ps1
+++ scriptCreator.ps1
@@ -7,16 +7,9 @@ $content = Get-Content -Raw -Path $filePath
 #initialise variable to store characters to print
 $output = ""
 
-#loop through each character in string
-#for ($i = 0; $i -lt $content.Length; $i += 2) {
-#	$output += $content[$i]
-#}
-
 for ($i = 0; $i -lt $content.Length -and $i -lt 64; $i += 2) {
     $output += $content[$i]
 }
 
-
-# print result
-Write-Output $output
-
+#save as .py file
+$output | Out-File -FilePath "helloWorld.py" -Force
