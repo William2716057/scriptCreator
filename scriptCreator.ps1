ccb135ecb44f186ab39125e8f1aacc00b8250b09
William <willspri@gmail.com>
2024-07-04 15:20:28 +1200

    committed

---
+++ scriptCreator.ps1
@@ -0,0 +1,17 @@
+#define path to text file
+$filePath = "sample.txt"
+
+#Read content of file as string
+$content = Get-Content -Raw -Path $filePath
+
+#initialise variable to store characters to print
+$output = ""
+
+#loop through each character in string
+for ($i = 0; $i -lt $content.Length; $i += 2) {
+	$output += $content[$i]
+}
+
+# print result
+Write-Output $output
+
